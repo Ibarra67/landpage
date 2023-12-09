078d72f3449cd5288d967a0a6c5b275fa7e7ae12
Ibarra67 <127575778+Ibarra67@users.noreply.github.com>
2023-12-09 16:47:48 +0800

    add new function to the button

--- src/components/forms/LoginForms.js
+++ src/components/forms/LoginForms.js
@@ -1,9 +1,57 @@
-import { View } from "react-native";
+import { View, ToastAndroid } from "react-native";
 import React from "react";
-import { Button, Text, TextInput, TouchableOpacity } from "react-native-paper";
+import { Button, Text, TextInput, HelperText } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import fetchServices from "../services/fetchServices";
 
-export default function LoginForms({ navigation }) {
+export default function LoginForm({ navigation }) {
   const [showPass, setShowPass] = React.useState(false);
+
+  const showToast = (message = "Something wen't wrong") => {
+    ToastAndroid.show(message, 3000);
+  };
+  
+  const handleLogin = async (values) => {
+    try { 
+      const url = "http://192.168.0.1/api/v1/login";
+      const result = await fetchServices.postData(url, values);
+
+      if (result.message != null) {
+        showToast(result?.message);
+      } else {
+        navigation.navigate("Home");
+      }
+    } catch (e) {
+      console.debug(e.toString());
+    }
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid Email")
+      .required("Please enter your email"),
+    password: Yup.string().required("Please enter your password"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={async (values) => {
+        await handleLogin(values);
+      }}
+      validationSchema={validationSchema}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        errors,
+        touched,
+        setTouched,
+      }) => {
   return (
     <View styles={{ flex: 1}}>
       <Text 
@@ -15,8 +63,21 @@ export default function LoginForms({ navigation }) {
         placeholder="Email"
         label="Email"
         style={{ marginTop: 10 }}
-        error={true}
-      />
+        defaultValue={values.email}
+              value={values.email}
+              keyboardType="email-address"
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              error={errors.email && touched.email}
+              onFocus={() => setTouched({ email: true }, false)}
+        />
+
+      {errors.email && touched.email && (
+              <HelperText type="error" visible={errors.email}>
+                {errors.email}
+              </HelperText>
+      )}
+
       <TextInput
         mode="outlined"
         placeholder="Password"
@@ -29,19 +90,50 @@ export default function LoginForms({ navigation }) {
           />
         }
         style={{ marginTop: 10 }}
-      />
+        value={values.password}
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              error={errors.password && touched.password}
+              onFocus={() => setTouched({ password: true }, false)}
+        />
+        {errors.password && touched.password && (
+              <HelperText type="error" visible={errors.password}>
+                {errors.password}
+              </HelperText>
+        )}
 
-      <Text>Forgot your <Button onPress={() => navigation.navigate("ForgotForms")}>Password</Button></Text>
+      <View
+        style={{
+          marginTop: 10,
+          alignItems: 'row',
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+        }}>
+      <Text>Forgot your <Text style={{color:"blue", textDecorationLine: "underline"}} onPress={() => navigation.navigate("ForgotForms")}>Password</Text></Text>
+      </View>
 
-      <Button icon="login" mode="contained" style={{ marginTop: 10, borderRadius: 5, }}>
-        Login
+      <Button 
+       loading={isSubmitting}
+       disabled={isSubmitting}
+       onPress={handleSubmit}
+       icon="login"
+       mode="contained"
+       style={{ marginTop: 10 }}
+      >
+       Login
       </Button>
 
-      <Text> Don't have any account ?
-      <Button onPress={() => navigation.navigate("Register")}>
+      <Text style={{marginTop: 10,}}> Don't have any account ?
+      <Text style={{color:"blue", textDecorationLine: "underline",}} disabled={isSubmitting}
+              onPress={() => navigation.navigate("Register")}
+              icon="account-plus"
+              mode="contained">
         Signup
-      </Button>
+      </Text>
       </Text>
     </View>
   );
+}}
+</Formik>
+);
 }
--- src/components/forms/SignupForms.js
+++ src/components/forms/SignupForms.js
@@ -1,10 +1,60 @@
-import { View } from 'react-native';
-import React from 'react';
-import { Button, Text, TextInput } from 'react-native-paper';
+import { View, ToastAndroid } from "react-native";
+import React from "react";
+import { Button, Text, TextInput } from "react-native-paper";
+import fetchServices from "../services/fetchServices";
 
-export default function SignupForms({navigation}) {
-    const [showPass, setShowPass] = React.useState(false);
-    const [showRePass, setShowRePass] = React.useState(false);
+export default function SignupForm({ navigation }) {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [repassword, setRepassword] = React.useState("");
+  const [showPass, setShowPass] = React.useState(false);
+  const [showRePass, setShowRePass] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  const showToast = (message = "Something wen't wrong") => {
+    ToastAndroid.show(message, 3000);
+  };
+
+  const handleRegistration = async () => {
+    try {
+      setLoading(true);
+
+      if (name === "" || email === "" || password === "" || repassword === "") {
+        showToast("Please input required data");
+        setIsError(true);
+        return false;
+      }
+
+      if (password !== repassword) {
+        showToast("Please match the password");
+        setIsError(true);
+        return false;
+      }
+
+      const url = "http://192.168.0.1;/api/v1/register";
+      const data = {
+        name,
+        email,
+        password,
+        password_confirmation: repassword,
+      };
+ 
+
+      const result = await fetchServices.postData(url, data);
+      
+      if (result?.message != null) {
+        showToast(result?.message);
+      } else {
+        navigation.navigate("Login");
+      }
+    } catch (e) {  
+      showToast(e.toString());
+    } finally {
+      setLoading(false);
+    }
+  };
         return (
             <View style={{flex: 1,justifyContent: "center"}}>
                 <Text style={{fontSize: 25,}}>Create an Account</Text>
@@ -13,44 +63,64 @@ export default function SignupForms({navigation}) {
                 placeholder='Name'
                 label='Name'
                 style={{ marginTop: 10}}
-                error={true}/>
+                value={name}
+                onChangeText={setName}
+                error={isError}
+                />
                 
                 <TextInput
                 mode='outlined'
                 placeholder='Email'
                 label='Email'
                 style={{ marginTop: 10}}
-                error={true}/>
+                value={email}
+                onChangeText={setEmail}
+                error={isError}
+                />
 
                 <TextInput
-                mode='outlined'
-                placeholder='Enter Password'
-                label='Enter Password'
-                secureTextEntry={showRePass}
-                right={
+                    mode="outlined"
+                    placeholder="Enter Password"
+                    label="Enter Password"
+                    secureTextEntry={!showPass}
+                    right={
                     <TextInput.Icon
-                    icon={!showPass ? 'eye' : 'eye-off'}
-                    onPress={() => setShowPass(!showPass)}/>
-                }
-                style={{marginTop: 10}}/>
+                        icon={showPass ? "eye" : "eye-off"}
+                        onPress={() => setShowPass(!showPass)}
+                    />
+                    }
+                    style={{ marginTop: 10 }}
+                    value={password}
+                    onChangeText={setPassword}
+                    error={isError}
+                    />
 
                 <TextInput
                 mode='outlined'
                 placeholder='Confirm Password'
                 label='Confirm Password'
-                secureTextEntry={showRePass}
+                secureTextEntry={!showRePass}
                 right={
-                    <TextInput.Icon
-                    icon={!showPass ? 'eye' : 'eye-off'}
-                    onPress={() => setShowPass(!showPass)}/>
+                <TextInput.Icon
+                    icon={showPass ? "eye" : "eye-off"}
+                    onPress={() => setShowRePass(!showRePass)}
+                />
                 }
-                style={{marginTop: 10}}/>
+                style={{ marginTop: 10 }}
+                value={repassword}
+                onChangeText={setRepassword}
+                error={isError}
+                />
 
-                <Button icon='account-plus' mode='contained' style={{ marginTop: 10, borderRadius: 5,}}>
+                <Button
+                disabled={loading}
+                loading={loading}
+                onPress={handleRegistration}
+                icon='account-plus' mode='contained' style={{ marginTop: 10, borderRadius: 5,}}>
                     Register
                 </Button>
 
-                <Text variant='titleSmall' style={{marginTop: 20,}}>Already have an account? <Button onPress={() => navigation.pop()}>Login</Button></Text>
+                <Text variant='titleSmall' style={{marginTop: 20,}}>Already have an account? <Text style={{color:"blue", textDecorationLine: "underline"}} onPress={() => navigation.pop()}>Login</Text></Text>
 
             </View>
         );
